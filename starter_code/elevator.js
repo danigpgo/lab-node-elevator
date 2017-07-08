d8da9b5f6e89f8f9463ea79513e07062ab5b3512
Danigp <danielentj@gmail.com>
2017-07-08 17:40:24 +0200

    Iteration 1 Done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,33 @@
 class Elevator {
   constructor(){
+    this.interval = undefined;
+    this.direction  = "none";
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.stop();
+    this.interval = setInterval(() => this.update(), 1000);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  update() {
+   this.log();
+ }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+
+    log() {
+      console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+    }
 }
 
 module.exports = Elevator;
