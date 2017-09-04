2cd02154fd18e89630e84985ac41ab36ada13bf5
Danigp <danielentj@gmail.com>
2017-09-04 19:18:23 +0200

    DONE

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,39 +1,94 @@
+
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
-    this.interval = undefined;
-    this.direction  = "none";
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "up";
+    this.int = 0;
+    this.waitingList = [];
+    this.passengers = [];
   }
 
   start() {
-    this.stop();
-    this.interval = setInterval(() => this.update(), 1000);
+    this.int = setInterval(() => this.update(), 1000);
   }
 
   stop() {
-    clearInterval(this.interval);
+    clearInterval(this.int);
   }
 
   update() {
-   this.log();
- }
-  _passengersEnter() { }
-  _passengersLeave() { }
+    if (this.requests[0] > this.floor){
+      this.floorUp();
+    }
+    else if (this.requests[0] < this.floor){
+      this.floorDown();
+    }
+    else if (this.requests[0] == this.floor){
+        this._passengersEnter();
+        this.requests = [];
+    }
+    if (!this.requests.length){
+      this._passengersLeave();
+    }
+  }
+
+  _passengersEnter() {
+      this.passengers.push(this.waitingList[0]);
+      this.waitingList.pop();
+      console.log("PASSENGERS: " + this.passengers[0].name);
+      this._passengersLeave();
+  }
+
+  _passengersLeave() {
+
+    if (this.passengers[0].destinationFloor < this.floor){
+      this.floorDown();
+    }
+    else if (this.passengers[0].destinationFloor > this.floor){
+      this.floorUp();
+    }
+    else if (this.passengers[0].destinationFloor == this.floor){
+      this.stop();
+      this.passengers.pop();
+      console.log ("HAS LLEGADO A TU DESTINO: " + this.floor);
+
+      console.log("EL ASCENSOR SE HA PARADO EN EL PISO: "+ this.floor);
+    }
+  }
 
   floorUp() {
-    if (this.floor < this.MAXFLOOR) this.floor++;
-  }
-  floorDown() {
-    if (this.floor > 0) this.floor--;
-  }
-
-  call() { }
-
-    log() {
-      console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+    this.direction = "up";
+    if (this.floor < this.MAXFLOOR) {
+      this.floor += 1;
+      this.log();
+    } else {
+      console.log("You're in the highest floor");
     }
+  }
+
+  floorDown() {
+    this.direction = "down";
+    if (this.floor > 0) {
+      this.floor -= 1;
+      this.log();
+    } else {
+      console.log("You're in the lowest floor");
+    }
+  }
+
+  call(person) {
+
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
+    this.start();
+  }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
+
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,2 +1,14 @@
 const Elevator = require('./elevator.js');
 const Person = require('./person.js');
+
+
+let elev = new Elevator();
+let pers = new Person("Dani", 4, 6);
+
+elev.call(pers)
+
+
+/* El ascensor arranca con la función call, acude a la petición,
+recoge al pasajero, lo lleva hasta su piso y se para - Queda pendiente
+resolver que puedan entrar más de un pasajero y que se puedan realizar
+más de una llamada sin que pete - Estamos trabajando en ello. */
